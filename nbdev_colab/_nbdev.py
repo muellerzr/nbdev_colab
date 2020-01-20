042bb1654df4b543b1327acd08bda77fafd819cd
muellerzr <muellerzr@gmail.com>
2020-01-20 01:47:30 +0000

    With_hooks

--- nbdev_colab/_nbdev.py
+++ nbdev_colab/_nbdev.py
@@ -2,9 +2,11 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"setup_drive": "00_core.ipynb",
+         "setup_git": "00_core.ipynb",
+         "git_push": "00_core.ipynb"}
 
-modules = []
+modules = ["core.py"]
 
 doc_url = "https://muellerzr.github.io/nbdev_colab/"
 
